6b79c64e776e801c5658b84564cbeb80fdf6a555
lmn <lalala@lala.com>
2019-03-23 17:37:56 +0800

    V 0.01 雏形
    完成了 基本数据结构
    处于 demo状态

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, flash, redirect
+from flask import url_for, request, send_from_directory
+
+app = Flask(__name__)
+app.secret_key = 'lalalalololo'
+app.config.from_pyfile('settings.py')
+
+
+def load_data(fp):
+    import json
+
+    def get_data(data):
+        for i in data:
+            path.append(i)
+            if isinstance(data[i], list):
+                for ii in data[i]:
+                    things.append(ii.split(',') + ["的".join(path)])
+            else:
+                get_data(data[i])
+            path.pop()
+
+    path = []
+    things = []
+    with open(fp, encoding="utf-8") as f:
+        data = json.load(f)
+
+    get_data(data)
+    return things
+
+
+@app.route('/')
+def index():
+    things = load_data("data/data.json")
+    find = request.args.get('find')
+    result = [i[-1] for i in things if i[0] == find]
+    if result == []:
+        return '哪里我也不知道'
+    else:
+        return '，和'.join(result)
